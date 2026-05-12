f2a4aea9936d9f8a458d8c7067876bca5322f09d
Ivan <int021h@gmail.com>
2026-05-12 19:42:34 +0200

    Update hwa_web_macro_runner.js

--- hwa_web_macro_runner.js
+++ hwa_web_macro_runner.js
@@ -1,4 +1,4 @@
-/// VERSION 1.0.0
+/// VERSION 1.0.1
 /// MACRO RUNNER - PASTE THIS CODE IN CONSOLE ONLY ONCE
 /// Send your ideas for improvements to Deidara/Phoenix Rebirth at Discord: @int021h
 const DEBUG_CLICKS = false
@@ -15,6 +15,8 @@ const actionWaitForColor = 5
 
 // Pixel color picker
 
+const colorsMatchThreshold = 15
+
 const originalRAF = window.requestAnimationFrame
 const gl = gameCanvas.getContext('webgl2')
 const pixels = new Uint8Array(4)
@@ -23,6 +25,7 @@ var readX = 0
 var readY = 0
 
 var isRunningMacro = false
+var lvlTitle = ""
 
 window.requestAnimationFrame = function(callback) {
     return originalRAF.call(this, function(time) {
@@ -74,7 +77,7 @@ function getColorCategory(pixel) {
     return 3 // blue
 }
 
-function colorsAreSame(color1, color2, threshold = 10) {
+function colorsAreSame(color1, color2, threshold = colorsMatchThreshold) {
     if (Math.abs(color1[0] - color2[0]) > threshold) return false
     if (Math.abs(color1[1] - color2[1]) > threshold) return false
     if (Math.abs(color1[2] - color2[2]) > threshold) return false
@@ -96,7 +99,7 @@ function addDebugToolbar() {
     const roomDecision = document.createElement('span')
     roomDecision.id = 'roomDecision'
     roomDecision.textContent = ' ? '
-
+    
     const roomRight = document.createElement('span')
     roomRight.id = 'roomRight'
     roomRight.textContent = 'RIGHT'
@@ -104,32 +107,29 @@ function addDebugToolbar() {
     container.appendChild(button)
     container.appendChild(document.createTextNode(' '))
     container.appendChild(roomLeft)
+    container.appendChild(document.createTextNode(' '))
     container.appendChild(roomDecision)
+    container.appendChild(document.createTextNode(' '))
     container.appendChild(roomRight)
     header.insertBefore(container, header.children[1])
 }
 
-var lvlTitle = ""
 /// MACRO RUNNER
-async function runActions(actions, options = {}) {
-    const {
-        target = gameCanvas || document
-    } = options
+async function runActions(actions) {
+    const target = gameCanvas
 
     const sleep = ms => new Promise(r => setTimeout(r, ms))
     var skipActions = 0
-    console.time('action')
     var prevClickAction = actions[0]
     for (const action of actions) {
-        console.timeEnd('action')
         if (!isRunningMacro) return
-        console.time('action')
-	if (skipActions > 0) {
+        
+        if (skipActions > 0) {
             skipActions--
-	    console.log("Skipping left room: ", JSON.stringify(action))
+            console.log("Skipping left room: ", JSON.stringify(action))
             continue
         }
-	console.log(JSON.stringify(action))
+        console.log(JSON.stringify(action))
         const {
             x = 0,
             y = 0,
@@ -142,25 +142,25 @@ async function runActions(actions, options = {}) {
 
         if (actionType == actionTitle) {
             lvlTitle = title
-	    continue
-	}
-
-        if (title != "") {
-	    document.title = lvlTitle + ": " + title
+            continue
         }
 
-	if (actionType == actionDelay) {
+        if (title != "") {
+            document.title = lvlTitle + ": " + title
+        }
+
+        if (actionType == actionDelay) {
             if (delay > 0) {
                 await sleep(delay)
             }
         } else if (actionType == actionWaitForColor) {
-            var retries = 2
+            var retries = 1
             var maxDelay = 10000
             var testPixel = []
             do {
-		await sleep(delay)
-		maxDelay -= delay
-		
+                await sleep(delay)
+                maxDelay -= delay
+        
                 testPixel = await readPixelOnDraw(
                     gameArea.width * x * canvasScaleX,
                     (gameArea.height * y) * canvasScaleY,
@@ -175,38 +175,38 @@ async function runActions(actions, options = {}) {
                     } else {
                          console.log("Colors didn't match after 10secs (skipping check): ", testPixel[0], testPixel[1], testPixel[2], "!=", color[0], color[1], color[2])
                          break
-		    }
+                    }
                 }
-            } while (!colorsAreSame(testPixel, color, 15) && isRunningMacro)
-	    await sleep(delay)
+            } while (!colorsAreSame(testPixel, color, colorsMatchThreshold) && isRunningMacro)
+            await sleep(delay)
         } else if (actionType == actionChooseRoom) {
             var leftPixel = await readPixelOnDraw(
                 gameArea.width * x * canvasScaleX,
                 gameArea.height * y * canvasScaleY,
             )
             var leftCategory = getColorCategory(leftPixel)
-	    var leftColor = `rgb(${leftPixel[0]}, ${leftPixel[1]}, ${leftPixel[2]})`
+            var leftColor = `rgb(${leftPixel[0]}, ${leftPixel[1]}, ${leftPixel[2]})`
 
             var rightPixel = await readPixelOnDraw(
                 gameArea.width * altX * canvasScaleX,
                 gameArea.height * y * canvasScaleY,
             )
             var rightCategory = getColorCategory(rightPixel)
-	    var rightColor = `rgb(${rightPixel[0]}, ${rightPixel[1]}, ${rightPixel[2]})`
+            var rightColor = `rgb(${rightPixel[0]}, ${rightPixel[1]}, ${rightPixel[2]})`
 
-	    roomLeft.style.color = leftColor
-	    roomRight.style.color = rightColor
-	    roomLeft.textContent = getElementName(leftCategory)
-	    roomRight.textContent = getElementName(rightCategory)
+            roomLeft.style.color = leftColor
+            roomRight.style.color = rightColor
+            roomLeft.textContent = getElementName(leftCategory)
+            roomRight.textContent = getElementName(rightCategory)
 
             if (rightCategory >= leftCategory) {
                 skipActions = 1
-  	        roomDecision.textContent = "->"
+                roomDecision.textContent = "->"
             } else {
-		roomDecision.textContent = "<-"
+                roomDecision.textContent = "<-"
             }
         } else if (actionType == actionClick) {
-	    gameCanvas.focus()
+            target.focus()
             prevClickAction = action
             const touch = new Touch({
                 identifier: Date.now(),
@@ -246,28 +246,28 @@ async function runActions(actions, options = {}) {
 }
 
 async function logMouse(e) {
-  const gameX = e.clientX - gameArea.x
-  const gameY = e.clientY - gameArea.y
+    const gameX = e.clientX - gameArea.x
+    const gameY = e.clientY - gameArea.y
 
-  const sleep = ms => new Promise(r => setTimeout(r, ms))
-  await sleep(1000)
+    const sleep = ms => new Promise(r => setTimeout(r, ms))
+    await sleep(1000)
 
-  const pixel = await readPixelOnDraw(gameX * canvasScaleX, gameY * canvasScaleY, 100)
-  const r = pixel[0]
-  const g = pixel[1]
-  const b = pixel[2]
-  const a = pixel[3]
+    const pixel = await readPixelOnDraw(gameX * canvasScaleX, gameY * canvasScaleY, 100)
+    const r = pixel[0]
+    const g = pixel[1]
+    const b = pixel[2]
+    const a = pixel[3]
 
-  const clickObj = {
-    x: gameX / gameArea.width,
-    y: gameY / gameArea.height,
-    color: [r,g,b],
-    delay: 100,
-    action: actionClick
-  }
+    const clickObj = {
+        x: gameX / gameArea.width,
+        y: gameY / gameArea.height,
+        color: [r,g,b],
+        delay: 100,
+        action: actionClick
+    }
 
-  console.log(JSON.stringify(clickObj))
-  document.body.style.setProperty('--bg-color', `rgba(${r}, ${g}, ${b}, ${a / 255})`)
+    console.log(JSON.stringify(clickObj))
+    document.body.style.setProperty('--bg-color', `rgba(${r}, ${g}, ${b}, ${a / 255})`)
 }
 if (DEBUG_CLICKS) gameCanvas.addEventListener('click', logMouse)
 
@@ -286,209 +286,209 @@ async function runDungeonMacro() {
         isRunningMacro = true
     }
 
-	
+    
     const someDelay = { 
-		x: 2, 
-		y: 2, 
-		delay: 2000, 
-		actionType: actionDelay
-	}
+        x: 2, 
+        y: 2, 
+        delay: 2000, 
+        actionType: actionDelay
+    }
 
     // ======= dungeon gates ======= 
     const waitForGateRight = {
-		x: 0.8378378378378378,
-		y: 0.21184510250569477,
-		color: [49,49,65], 
-		delay: 100, 
-		actionType: actionWaitForColor, 
-		title: "waiting for right gate"
-	}
+        x: 0.8378378378378378,
+        y: 0.21184510250569477,
+        color: [49,49,65], 
+        delay: 100, 
+        actionType: actionWaitForColor, 
+        title: "waiting for right gate scene"
+    }
     const waitForGate10 = {
-		x: 0.3492723492723493, 
-		y:0.30068337129840544, 
-		color: [48,54,60], 
-		delay: 100, 
-		actionType: actionWaitForColor, 
-		title: "waiting for floor2 gate10"
-	}
+        x: 0.3492723492723493, 
+        y:0.30068337129840544, 
+        color: [48,54,60], 
+        delay: 100, 
+        actionType: actionWaitForColor, 
+        title: "waiting for floor2 gate10 scene"
+    }
     const gateRight = {
-		x:0.691268,
-		y:0.5, 
-		delay: 500, 
-		actionType: actionClick, 
-		title: "clicking on right gate"
-	}
+        x:0.691268,
+        y:0.5, 
+        delay: 500, 
+        actionType: actionClick, 
+        title: "clicking on right gate"
+    }
 
     const waitForGateMid = {
-		x: 0.6424116424116424, 
-		y: 0.21184510250569477, 
-		color: [49,49,65], 
-		delay:100, 
-		actionType: actionWaitForColor, 
-		title: "waiting for mid gate"
-	}
+        x: 0.6424116424116424, 
+        y: 0.21184510250569477, 
+        color: [49,49,65], 
+        delay:100, 
+        actionType: actionWaitForColor, 
+        title: "waiting for mid gate scene"
+    }
     const gateMid = {
-		x: 0.500, 
-		y: 0.5, 
-		delay: 500, 
-		actionType: actionClick, 
-		title: "clicking on mid gate"
-	}
+        x: 0.500, 
+        y: 0.5, 
+        delay: 500, 
+        actionType: actionClick, 
+        title: "clicking on mid gate"
+    }
 
     const waitForGateLeft = {
-		x: 0.16112266112266113, 
-		y: 0.21184510250569477, 
-		color: [49,49,65], 
-		delay: 100, 
-		actionType: actionWaitForColor, 
-		title: "waiting for left gate"
-	}
+        x: 0.16112266112266113, 
+        y: 0.21184510250569477, 
+        color: [49,49,65], 
+        delay: 100, 
+        actionType: actionWaitForColor, 
+        title: "waiting for left gate scene"
+    }
     const gateLeft = {
-		x: 0.312, 
-		y: 0.5, 
-		delay: 500, 
-		actionType: actionClick, 
-		title: "clicking on left gate"
-	}
+        x: 0.312, 
+        y: 0.5, 
+        delay: 500, 
+        actionType: actionClick, 
+        title: "clicking on left gate"
+    }
 
     // ======= dungeon elemental rooms ======= 
     const waitFor1RoomSelection = {
-		x: 0.65488565, 
-		y: 0.1708428,
-		color: [19,15,7], 
-		delay: 100, 
-		actionType: actionWaitForColor, 
-		title: "waiting for room selection"
-	}
+        x: 0.65488565, 
+        y: 0.1708428,
+        color: [19,15,7], 
+        delay: 100, 
+        actionType: actionWaitForColor, 
+        title: "waiting for single room selection popup"
+    }
     const waitFor2RoomSelection = {
-		x: 0.5, 
-		y: 0.5, 
-		color: [19,17,7], 
-		delay: 100, 
-		actionType: actionWaitForColor, 
-		title: "waiting for room selection"
-	}
+        x: 0.5, 
+        y: 0.5, 
+        color: [19,17,7], 
+        delay: 100, 
+        actionType: actionWaitForColor, 
+        title: "waiting for double room selection popup"
+    }
     const roomMid = {
-		x: 0.5, 
-		y: 0.795, 
-		delay: 500, 
-		actionType: actionClick, 
-		title: "clicking on mid room"
-	}
+        x: 0.5, 
+        y: 0.795, 
+        delay: 500, 
+        actionType: actionClick, 
+        title: "clicking on mid room"
+    }
 
     //  ======= usage: checkRoomColors, roomLeft, roomRight ======= 
     const checkRoomColors = {
-		x: 0.31496881496881496, 
-		y: 0.6560364464692483,
-		altX: 0.6891891891891891,
-		delay: 100, 
-		actionType: actionChooseRoom, 
-		title: "choosing a room"
-	}
+        x: 0.31496881496881496, 
+        y: 0.6560364464692483,
+        altX: 0.6891891891891891,
+        delay: 100, 
+        actionType: actionChooseRoom, 
+        title: "choosing a correct room"
+    }
     const roomLeft = {
-		x: 0.3076, 
-		y: 0.8,
-		delay: 500, 
-		actionType: actionClick,
-		title: "clicking on left room"
-	}
+        x: 0.3076, 
+        y: 0.8,
+        delay: 500, 
+        actionType: actionClick,
+        title: "clicking on left room"
+    }
     const roomRight = {
-		x: 0.6833, 
-		y: 0.8, 
-		delay: 500, 
-		actionType: actionClick,
-		title: "clicking on right room"
-	}
+        x: 0.6833, 
+        y: 0.8, 
+        delay: 500, 
+        actionType: actionClick,
+        title: "clicking on right room"
+    }
     // ======= dungeon battlefield screen ======= 
 
     const waitForBattlefield = {
-		x: 0.014553, 
-		y: 0.952164, 
-		color: [34,46,64], 
-		delay: 100, 
-		actionType: actionWaitForColor, 
-		title: "waiting for battlefield"
-	}
+        x: 0.014553, 
+        y: 0.952164, 
+        color: [34,46,64], 
+        delay: 100, 
+        actionType: actionWaitForColor, 
+        title: "waiting for battlefield scene"
+    }
     const autoBattle = {
-		x: 0.87214, 
-		y: 0.758542, 
-		delay: 500, 
-		actionType: actionClick, 
-		title: "clicking autobattle"
-	}
+        x: 0.87214, 
+        y: 0.758542, 
+        delay: 500, 
+        actionType: actionClick, 
+        title: "clicking autobattle"
+    }
 
     // ======= dungeon confirm auto-battle results screen ======= 
     const waitForConfirmBattle = {
-		x: 0.60083, 
-		y: 0.127563, 
-		color: [137,1,0], 
-		delay:100, 
-		actionType: actionWaitForColor, 
-		title: "waiting for battle result"
-	}
+        x: 0.60083, 
+        y: 0.127563, 
+        color: [137,1,0], 
+        delay:100, 
+        actionType: actionWaitForColor, 
+        title: "waiting for battle result popup"
+    }
     const confirmBattle = {
-		x: 0.641372, 
-		y: 0.822323, 
-		delay: 1000, 
-		actionType: actionClick, 
-		title: "clicking on confirm battle result"
-	}
+        x: 0.641372, 
+        y: 0.822323, 
+        delay: 1000, 
+        actionType: actionClick, 
+        title: "clicking on confirm battle result"
+    }
 
     // ======= dungeon floor finished symbol ======= 
     const waitForFloor1Done = {
-		x:0.6496881496881497, 
-		y:0.30068337129840544, 
-		color: [51,54,60], 
-		delay: 100, 
-		actionType: actionWaitForColor,
-		title: "waiting for floor1 finish symbol"
-	}
+        x:0.6496881496881497, 
+        y:0.30068337129840544, 
+        color: [51,54,60], 
+        delay: 100, 
+        actionType: actionWaitForColor,
+        title: "waiting for floor1 final scene"
+    }
     const floor1Done = {
-		x: 0.7297, 
-		y: 0.47836, 
-		delay: 1000, 
-		actionType: actionClick, 
-		title: "clicking on floor1 finish symbol"
-	}
+        x: 0.7297, 
+        y: 0.47836, 
+        delay: 1000, 
+        actionType: actionClick, 
+        title: "clicking on floor1 final symbol"
+    }
 
     const waitForFloor2Done = {
-		x: 0.3492723492723493, 
-		y: 0.30068337129840544, 
-		color: [48,54,60], 
-		delay: 100, 
-		actionType: actionWaitForColor, 
-		title: "waiting for floor2 finish symbol"
-	}
+        x: 0.3492723492723493, 
+        y: 0.30068337129840544, 
+        color: [48,54,60], 
+        delay: 100, 
+        actionType: actionWaitForColor, 
+        title: "waiting for floor2 final scene"
+    }
     const floor2Done = {
-		x: 0.27755, 
-		y: 0.47836, 
-		delay: 1000, 
-		actionType: actionClick, 
-		title: "clicking on floor2 finish symbol"
-	}
+        x: 0.27755, 
+        y: 0.47836, 
+        delay: 1000, 
+        actionType: actionClick, 
+        title: "clicking on floor2 final symbol"
+    }
     // ======= dungeon floor finished popup ========
 
     const waitForFloorConfirm = {
-		x: 0.5, 
-		y: 0.5, 
-		color: [22,12,8], 
-		delay: 100, 
-		actionType: actionWaitForColor,  
-		title: "waiting for floor finish confirmation"
-	}
+        x: 0.5, 
+        y: 0.5, 
+        color: [22,12,8], 
+        delay: 100, 
+        actionType: actionWaitForColor,  
+        title: "waiting for floor confirmation popup"
+    }
     const floorConfirm = {
-		x: 0.635, 
-		y: 0.697, 
-		delay: 4000, 
-		actionType: actionClick, 
-		title: "clicking on floor finish confirmation"
-	}
+        x: 0.635, 
+        y: 0.697, 
+        delay: 4000, 
+        actionType: actionClick, 
+        title: "clicking on floor confirmation popup"
+    }
     const floors = 200
 
     for (let i = 0; i < floors; i++) {
         if (!isRunningMacro) return
         await runActions([
-			someDelay, 
+            someDelay, 
             title("lvl1 single"), waitForGateRight, gateRight, waitFor1RoomSelection, roomMid, waitForBattlefield, autoBattle, waitForConfirmBattle, confirmBattle,
             title("lvl2 double"), waitForGateMid, gateMid, waitFor2RoomSelection, checkRoomColors, roomLeft, roomRight, waitForBattlefield, autoBattle, waitForConfirmBattle, confirmBattle,
             title("lvl3 double"), waitForGateMid, gateMid, waitFor2RoomSelection, checkRoomColors, roomLeft, roomRight, waitForBattlefield, autoBattle, waitForConfirmBattle, confirmBattle,
