cd881945c8265b7e6c7aa7405251df3b248cc74e
Ivan <int021h@gmail.com>
2026-08-09 13:05:12 +0200

    Update hwa_extension.js

--- hwa_extension.js
+++ hwa_extension.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dungeon runner
 // @namespace    http://tampermonkey.net/
-// @version      2026-07-13
+// @version      2026-08-09
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.hero-wars-alliance.com/*
